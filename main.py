0c62202d441a22d9a66ddfbbf9dbd1fdcff42ecb
MathiTC <mtcskole@gmail.com>
2023-01-13 13:48:22 +0100

    Beskrivelse skrevet

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta #henter tiden
 
-sleep_hours = int(input("How many hours do you want to sleep? "))
-current_time = datetime.now()
-wakeup_time = current_time + timedelta(hours=sleep_hours)
+sleep_hours = int(input("How many hours do you want to sleep? ")) #Spørg for tal som indsættes til variablen
+current_time = datetime.now() #Nuværende tid kobles til variablen
+wakeup_time = current_time + timedelta(hours=sleep_hours) #Beregner hvornår du vågner igen.
 
-print("Current time:", current_time.strftime("%H:%M:%S"))
-print("Wakeup time:", wakeup_time.strftime("%H:%M:%S"))
+print("Current time:", current_time.strftime("%H:%M:%S")) #Printer nuværende tidspunkt
+print("Wakeup time:", wakeup_time.strftime("%H:%M:%S")) #Printer tiden efter du har sovet.
